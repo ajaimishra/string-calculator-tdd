828825e684400e9b7e7210c0fc573420d2127fe9
ajaimishra <akm.r.mishra@gmail.com>
2025-06-25 20:45:54 +0530

    add implimentation to handle test case for comma separated input

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,8 +1,12 @@
 export class StringCalculator {
 
-  add(numbers: string) : number {
-    if(numbers.trim() === '') return 0;
+  add(numbers: string): number {
+    if (numbers.trim() === '') return 0;
 
+    if (numbers.includes(',')) {
+      const numbersArray = numbers.split(',').map(num => parseInt(num.trim()));
+      return numbersArray.reduce((acc, num) => acc + num, 0)
+    }
     return parseInt(numbers);
   }
 }
