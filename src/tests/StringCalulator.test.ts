215911406e49c08defeca85e75e22e867fec1bfa
ajaimishra <akm.r.mishra@gmail.com>
2025-06-25 20:40:06 +0530

    test case without implimentation to handle the comma sepaarted input

--- src/tests/StringCalulator.test.ts
+++ src/tests/StringCalulator.test.ts
@@ -5,14 +5,19 @@ describe('StringCalculator Test cases', () => {
   let calculator = new StringCalculator();
 
   it('Should return 0 for empty string', () => {
-      expect(calculator.add('')).toBe(0)
-      expect(calculator.add('   ')).toBe(0)
+    expect(calculator.add('')).toBe(0)
+    expect(calculator.add('   ')).toBe(0)
   })
-  
+
   it('Should return input string as number for single input string', () => {
     expect(calculator.add('1')).toBe(1)
     expect(calculator.add('6')).toBe(6)
     expect(calculator.add('15')).toBe(15)
+  })
+
+  it('Should return sum of two numbers seprated by comma', () => {
+    expect(calculator.add('1,2')).toBe(3)
+    expect(calculator.add('3,4')).toBe(7)
   })
 
 })
