607e23878afc41904e503d71d44114543d7e79ba
ajaimishra <akm.r.mishra@gmail.com>
2025-06-26 00:06:30 +0530

    add test for custom delimeter and make test pass

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -3,14 +3,28 @@ export class StringCalculator {
   add(numbers: string): number {
     if (numbers.trim() === '') return 0;
 
-    const numbersArray = this.parseNumbers(numbers);
+    const { delimeter, numberString } = this.extractDelimeterAndNumbers(numbers);
+    const numbersArray = this.parseNumbers(numberString, delimeter);
+
     return numbersArray.reduce((acc, num) => acc + num, 0)
 
   }
 
+  private extractDelimeterAndNumbers(numbers: string): { delimeter: string, numberString: string } {
 
-  private parseNumbers(numbers: string): number[] {
-    const sanetizedNumbers = numbers.replace(/\n/g, ',')
-    return sanetizedNumbers.split(',').map(num => parseInt(num.trim()));
+    if (numbers.startsWith('//')) {
+      const delimeterEndIndex = numbers.indexOf('\n');
+      const delimeter = numbers.slice(2, delimeterEndIndex);
+      const numberString = numbers.substring(delimeterEndIndex + 1);
+      return { delimeter, numberString }
+    }
+    return { delimeter: ',', numberString: numbers }
+
+  }
+
+
+  private parseNumbers(numbers: string, delimeter: string): number[] {
+    const sanetizedNumbers = numbers.replace(/\n/g, delimeter)
+    return sanetizedNumbers.split(delimeter).map(num => parseInt(num.trim()));
   }
 }
--- src/tests/StringCalulator.test.ts
+++ src/tests/StringCalulator.test.ts
@@ -4,28 +4,37 @@ describe('StringCalculator Test cases', () => {
 
   let calculator = new StringCalculator();
 
-  it('Should return 0 for empty string', () => {
+  it('should return 0 for empty string', () => {
     expect(calculator.add('')).toBe(0)
     expect(calculator.add('   ')).toBe(0)
   })
 
-  it('Should return input string as number for single input string', () => {
+  it('should return input string as number for single input string', () => {
     expect(calculator.add('1')).toBe(1)
     expect(calculator.add('6')).toBe(6)
     expect(calculator.add('15')).toBe(15)
   })
 
-  it('Should return sum of two or more than two numbers seprated by comma', () => {
+  it('should return sum of two or more than two numbers seprated by comma', () => {
     expect(calculator.add('1,2')).toBe(3)
     expect(calculator.add('3,4')).toBe(7)
     expect(calculator.add('1,2,3')).toBe(6)
     expect(calculator.add('1,2,3,4,5,6,7,8')).toBe(36)
   })
 
-  it('Should handle newlines between numbers', () => {
+  it('should handle newlines between numbers', () => {
     expect(calculator.add('1\n2,3')).toBe(6)
     expect(calculator.add("1\n2\n3")).toBe(6)
 
   })
+
+  it('should support custom delimiters', () => {
+    expect(calculator.add('//;\n6;5')).toBe(11);
+    expect(calculator.add('//|\n4|5|6')).toBe(15);
+    expect(calculator.add('//#\n4#5#6')).toBe(15);
+  });
+
+
+
 
 })
