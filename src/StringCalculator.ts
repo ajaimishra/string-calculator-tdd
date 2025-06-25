df2560769702ea52a18d7fe6968f9643503f91f0
ajaimishra <akm.r.mishra@gmail.com>
2025-06-26 00:16:52 +0530

    make test pass for negative numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -5,7 +5,7 @@ export class StringCalculator {
 
     const { delimeter, numberString } = this.extractDelimeterAndNumbers(numbers);
     const numbersArray = this.parseNumbers(numberString, delimeter);
-
+    this.validateNumbers(numbersArray);
     return numbersArray.reduce((acc, num) => acc + num, 0)
 
   }
@@ -26,5 +26,12 @@ export class StringCalculator {
   private parseNumbers(numbers: string, delimeter: string): number[] {
     const sanetizedNumbers = numbers.replace(/\n/g, delimeter)
     return sanetizedNumbers.split(delimeter).map(num => parseInt(num.trim()));
+  }
+
+  private validateNumbers(numbers: number[]): void {
+    const negativeNumbers = numbers.filter(num => num < 0);
+    if (negativeNumbers.length) {
+      throw new Error(`negative numbers not allowed ${negativeNumbers.join(', ')}`)
+    }
   }
 }
